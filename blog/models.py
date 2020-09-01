ef73e4f14532649c6c120e10a18149d190e320ca
Suyeon Lee <Lsyeon@kookmin.ac.kr>
2020-09-01 18:09:32 +0900

    blog app - Post

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=30) #제목
+    content = models.TextField() #내용
+
+    created = models.DateTimeField() #작성일
+    author = models.ForeignKey(User, on_delete=models.CASCADE) #어떤 사용자가 삭제되면 글도 삭제
