a743ba30b58fe09fb649dafcc69099c9796f1e43
Suyeon Lee <Lsyeon@kookmin.ac.kr>
2020-09-01 19:03:41 +0900

    blog post list (index)

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+
+def index(request):
+    posts = Post.objects.all()
+
+    return render(
+        request,
+        'blog/index.html',
+        {
+            'posts': posts,
+        }
+    )
