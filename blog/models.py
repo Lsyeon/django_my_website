0cea06b19161ca968e8fa2d3e1e935b1037cc9f7
Suyeon Lee <Lsyeon@kookmin.ac.kr>
2020-09-03 04:12:38 +0900

    category num and display

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=25, unique=True)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Post(models.Model):
     title = models.CharField(max_length=30) #제목
     content = models.TextField() #내용
@@ -10,6 +18,8 @@ class Post(models.Model):
 
     created = models.DateTimeField() #작성일
     author = models.ForeignKey(User, on_delete=models.CASCADE) #어떤 사용자가 삭제되면 글도 삭제
+
+    category = models.ForeignKey(Category, blank=True, null=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return '{} :: {}'.format(self.title, self.author)
